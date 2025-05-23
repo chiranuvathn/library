33a56244ab5e703688f6628c40072886cfb06892
GearChira <chiranuvathn@gmail.com>
2025-05-23 23:41:15 +0700

    - init

---
+++ script.js
@@ -0,0 +1,22 @@
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+    this.id = crypto.randomUUID();
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${this.pages} pages, ${this.isRead}`
+    }
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+    let newBook = new Book(title, author, pages, isRead);
+    myLibrary.push(newBook);
+}
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
+addBookToLibrary("1984", "George Orwell", 328, true);
+
+console.log(myLibrary);
