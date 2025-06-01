52fe159c0b6fc8000b01054e5c3b87de8d8d149e
GearChira <chiranuvathn@gmail.com>
2025-06-01 22:00:43 +0700

    - wip

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const formSubmit = document.querySelector('.submit-button');
 
 function Book(title, author, pages, isRead) {
     this.id = crypto.randomUUID();
@@ -15,8 +16,13 @@ function addBookToLibrary(title, author, pages, isRead) {
     let newBook = new Book(title, author, pages, isRead);
     myLibrary.push(newBook);
 }
+	
+// addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
+// addBookToLibrary("1984", "George Orwell", 328, true);
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, false);
-addBookToLibrary("1984", "George Orwell", 328, true);
+function getFormInput() {
+   let formInput = document.querySelectorAll('[id^="input-"]'); // bug: got called before event trigger
+   console.log(formInput);
+}
 
-console.log(myLibrary);
+formSubmit.addEventListener('click', getFormInput());
